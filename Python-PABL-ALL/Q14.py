9ab4be9fa505ddd254746f4926ff5eb712945900
abhirajsingh <abhiraj.singh.cs28@iilm.edu>
2026-03-17 13:59:47 +0530

    Q14AddNumber.py

---
+++ Python-PABL-ALL/Q14.py
@@ -0,0 +1,13 @@
+class Solution{
+    public:
+    long long get_Sum(int n, vector<int>& input)
+    {
+        long long sum = 0;
+        
+        for(int i = 0; i < n; i++){
+            sum += input[i];
+        }
+        
+        return sum;
+    }
+};
